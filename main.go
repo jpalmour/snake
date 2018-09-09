5af5c6f10e59a65221bd6e05d2e67c358970b34e
Joseph Palmour <jpalmour@gmail.com>
2018-09-09 01:59:32 -0400

    Web assembly (#2)
    * begin wasm implementation
    
    * begin browser implementation
    
    * begin painting snake in css grid
    
    * create working display
    
    * update styles
    
    * update styles
    
    * update styles
    
    * add browser control

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/jpalmour/snake/go/browser"
+	"github.com/jpalmour/snake/go/game"
+)
+
+// build with `GOARCH=wasm GOOS=js go build -o snake.wasm main.go`
+func main() {
+	println("Starting Snake written in Go compiled to WebAssembly...")
+	c := browser.NewController()
+	d := browser.NewDisplay()
+	g := game.New(30, 150, c, d)
+	g.Play()
+}
